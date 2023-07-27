2773deccca29e0fb5bf40e02bbb668303b2ab761
Brian <wamwangimathenge@gmail.com>
2023-07-27 14:03:25 +0300

    Code a conditional logic that throws an error
    This commit throws an error if either of the arguments passed to the
    addTwoNums are not numbers

--- script.js
+++ script.js
@@ -7,13 +7,30 @@
 // addTwoNums(5, "5")
 
 // Task 3: Update the addTwoNums function with a try...catch block
+// function addTwoNums(a, b) {
+//     try {
+//         console.log(a + b)
+//     } catch(err) {
+//         console.log(err)
+//         console.log("The program continues running")
+//     }
+// }
+
+// addTwoNums(5, "5")
+
+// Task 4: If the passed-in arguments are not numbers, throw an error
 function addTwoNums(a, b) {
     try {
-        console.log(a + b)
+        if(typeof(a) != 'number') {
+            throw new ReferenceError("the first argument is not a number")
+        } else if(typeof(b) != 'number') {
+            throw new ReferenceError("the second argument is not a number")
+        } else {
+            console.log(a + b)
+        }
     } catch(err) {
         console.log(err)
-        console.log("The program continues running")
     }
 }
 
-addTwoNums(5, "5")
+addTwoNums("5", 5)
