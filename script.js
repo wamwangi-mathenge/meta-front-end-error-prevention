5fa539f42c277e98ce8bc753abe05eee7cb04929
Brian <wamwangimathenge@gmail.com>
2023-07-27 13:41:04 +0300

    Update the function with a try...catch block
    This commit adds the try and catch blocks inside the function
    definition's body.
    Additionally, the catch block catches an error named err and inside the
    body of the catch block, you need to console the err value.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Task 1: Code a function declaration named addTwoNums which accepts a and b and console logs a + b
-function addTwoNums(a, b) {
-    console.log(a + b);
-}
+// function addTwoNums(a, b) {
+//     console.log(a + b);
+// }
 
 // Task 2: Invoke the addTwoNums function with a number and a string
+// addTwoNums(5, "5")
+
+// Task 3: Update the addTwoNums function with a try...catch block
+function addTwoNums(a, b) {
+    try {
+        console.log(a + b)
+    } catch(err) {
+        console.log(err)
+        console.log("The program continues running")
+    }
+}
+
 addTwoNums(5, "5")
